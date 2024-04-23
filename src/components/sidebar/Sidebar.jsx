ea196f390fecdb5e1ed98b69fe5da88cf56e4cd8
ilhamdosantos <ilhamsantoso007@gmail.com>
2024-04-23 14:10:59 +0700

    ubah teks di app

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,63 @@
+import "./sidebar.scss"
+import { Link } from "react-router-dom";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import BorderStyleIcon from '@mui/icons-material/BorderStyle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const Sidebar = () => {
+  return (
+    
+    <div className="sidebar">
+  <div className="top">
+    <Link to="/">
+      <span className="logo">Store</span>
+    </Link>
+  </div>
+  <hr />
+  <div className="center">
+    <ul>
+      <p className="title">MAIN</p>
+      <li>
+        <DashboardIcon className="icon" />
+        <span>Dashboard</span>
+      </li>
+      <p className="title">LIST</p>
+      <Link to="/users">
+        <li>
+          <PersonIcon className="icon" />
+          <span>Users</span>
+        </li>
+      </Link>
+      <Link to="/products">
+        <li>
+            <ProductionQuantityLimitsIcon className="icon" />
+          <span>Products</span>
+        </li>
+      </Link>
+      <li>
+        <BorderStyleIcon className="icon" />
+        <span>Orders</span>
+      </li>
+      <p className="title">USER</p>
+      <li>
+        <AccountCircleIcon className="icon" />
+        <span>Profile</span>
+      </li>
+      <li>
+        <LogoutIcon className="icon" />
+        <span>Logout</span>
+      </li>
+    </ul>
+  </div>
+  <div className="bottom">
+    <div className="colorOption"></div>
+    <div className="colorOption"></div>
+  </div>
+</div>
+  )
+}
+
+export default Sidebar
